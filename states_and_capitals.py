9df019a40a40d87aca0bdf4b4c9c04a11fcbe896
Craig Hillelson <craig.hillelson@intersection.com>
2018-09-03 08:04:22 -0400

    Expanded the options available to the user. They can now be quizzed on state capitals, nicknames, and the year the state joined the union.

--- states_and_capitals.py
+++ states_and_capitals.py
@@ -2,9 +2,50 @@
 import csv
 import random
 
-# define function
+# define functions
 def print_return():
 	print("\n")
+
+def quiz(a, b):
+	# define variables
+	correct = 0
+	incorrect = 0
+	# for the number of states the user specified to drill, generate an equal number of random numbers less than 50
+	for j in i:
+		print_return()
+		state = states[j]
+		state_formatted = "%s " % (state)
+		# set 'a' equal to a random item in the chosen list (capitals, nicknames, or years_founded)
+		a = b[j]
+		user_answer = raw_input(state_formatted)
+		# test the user's answer and respond 
+		if user_answer == a:
+			print("Correct. Good job!\n")
+			correct = correct + 1
+			corrects.append(state)
+		else:
+			print("Sorry. That's incorrect.\n")
+			incorrect = incorrect + 1
+			incorrects.append(state)
+	# calculate and format percentage correct
+	percentage_correct =  float(correct) / float(user_choice)
+	percentage_formatted = "{0:.0%}".format(percentage_correct)
+	print("Results: %s correct %s incorrect. %s\n") % (correct, incorrect, percentage_formatted)
+	
+	# update user with their results. if user got any wrong, write the states missed to a text file
+	with open('results.txt', 'w') as results:
+		if incorrect > 0:
+			results.write("States to Review"+ "\n")
+			print("States to brush up on:")
+			for states_missed in incorrects:
+				results.write(states_missed+ "\n")
+				print(states_missed)
+			print("Check 'results.txt' to see where you can improve.")
+			print_return()
+		else:
+			results.write("Great job!!! 100%")
+			print("Great job!!! 100%")
+			print_return()
 
 # define lists to be populated later
 states = []
@@ -19,6 +60,9 @@ incorrects = []
 with open('states_and_capitals.csv') as f:
 	f_csv = csv.DictReader(f)
 	for row in f_csv:
+		capital = row['capital']
+		nickname = row['nickname']
+		year_founded = row['year founded']
 		states.append(row['state'])
 		capitals.append(row['capital'])
 		nicknames.append(row['nickname'])
@@ -29,14 +73,28 @@ with open('states_and_capitals.csv') as f:
 number_of_states = len(states)
 
 # define questions
-category = "Would you like to drill capitals, nicknames, or years founded? "
-# question = "How many states would you like to drill in that category? (select an integer between 1 and %s) " % (number_of_states)"
-question = "How many states and capitals would you like to drill? (select an integer between 1 and %s) " % (number_of_states)
+category = "Would you like to drill a - capitals, b - nicknames, or c - years founded? "
+question = "How many states would you like to drill in that category? (select an integer between 1 and %s) " % (number_of_states)
 
 # define responses
 too_many = "Please select an integer less than %s " % (number_of_states)
 not_an_integer = "That won't work. Please enter an integer between 1 and 50. "
 
+# prompt user to select a category
+while True:
+	category_choice = str(raw_input(category)).lower().strip()
+	if category_choice not in ['a', 'b', 'c']:
+		print("invalid choice")
+	else:
+		if category_choice == 'a':
+			print("you selected %s - capitals") % (category_choice)
+		elif category_choice == 'b':
+			print("you selected %s - nicknames") % (category_choice)
+		else:
+			print("you selected %s - years founded") % (category_choice)
+		break
+
+# prompt user to select how many states they'd like to drill
 while True:
 	try:
 		user_choice = int(raw_input(question))
@@ -48,41 +106,13 @@ while True:
 	else:
 		break
 
-# define i, set counters to 0
+# define i
 i = random.sample(range(number_of_states), int(user_choice))
-correct = 0
-incorrect = 0
 
 # prompt user
-for j in i:
-	print_return()
-	state = states[j]
-	state_formatted = "%s " % (state)
-	capital = capitals[j]
-	user_answer = raw_input(state_formatted)
-	if user_answer == capital:
-		print("Correct. Good job!\n")
-		correct = correct + 1
-	else:
-		print("Sorry. That's incorrect.\n")
-		incorrect = incorrect + 1
-		incorrects.append(state)
-
-# update user on their performance
-percentage_correct =  float(correct) / float(user_choice)
-percentage_formatted = "{0:.0%}".format(percentage_correct)
-print("Results: %s correct %s incorrect. %s\n") % (correct, incorrect, percentage_formatted)
-
-# let user know which states need some work
-print("States to brush up on:")
-for states in incorrects:
-	print(states)
-
-# write results to a text file
-with open('results.txt', 'w') as results:
-	results.write("States to Review"+ "\n")
-	for states in incorrects:
-		results.write(states+ '\n')
-
-# update user
-print("Check 'results.txt' to see where you can improve.")
+if category_choice == 'a':
+	quiz(capital, capitals)
+elif category_choice == 'b':
+	quiz(nickname, nicknames)
+else:
+	quiz(year_founded, years_founded)
--- user_validation.py
+++ user_validation.py
@@ -3,6 +3,7 @@
 
 # copied from github and altered
 
+'''
 def capitals_nicknames_years(question):
 	while True:
 		reply = str(raw_input(question+' (a - capitals, b - nicknames, c - years founded): ')).lower().strip()
@@ -18,3 +19,17 @@ def capitals_nicknames_years(question):
 			break
 			
 capitals_nicknames_years("Would you like to drill")
+'''
+
+while True:
+	reply = str(raw_input("Would you like to drill (a - capitals, b - nicknames, c - years founded): ")).lower().strip()
+	if reply not in ['a', 'b', 'c']:
+		print("invalid choice")
+	else:
+		if reply == 'a':
+			print("you selected %s - capitals") % (reply)
+		elif reply == 'b':
+			print("you selected %s - nicknames") % (reply)
+		elif reply == 'c':
+			print("you selected %s - years founded") % (reply)
+		break
